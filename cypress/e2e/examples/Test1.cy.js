cc37b970dcc2882343d290926c34181fdf0c0b98
nbhaohao <nbhaohao@163.com>
2022-08-10 11:26:53 +0800

    Try cypress thenable

--- cypress/e2e/examples/Test1.cy.js
+++ cypress/e2e/examples/Test1.cy.js
@@ -1,16 +1,21 @@
 describe("My First Test Suite", () => {
   it("My FirstTest case", () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-    cy.get('.search-keyword').type('ca')
-    cy.wait(1000)
-    cy.get('.product:visible').should('have.length', 4)
-    cy.get('.products').find('.product').should('have.length', 4)
+    cy.get(".search-keyword").type("ca");
+    cy.wait(1000);
+    cy.get(".product:visible").should("have.length", 4);
+    cy.get(".products").find(".product").should("have.length", 4);
 
-    cy.get('.products').find('.product').each(element => {
-      const textVegetable = element.find("h4.product-name").text()
-      if (textVegetable.includes("Cashews")) {
-        cy.wrap(element).find("button").click()
-      }
-    })
-  })
-})
+    cy.get(".products")
+      .find(".product")
+      .each((element) => {
+        const textVegetable = element.find("h4.product-name").text();
+        if (textVegetable.includes("Cashews")) {
+          cy.wrap(element).find("button").click();
+        }
+      });
+    cy.get(".brand").then((element) => {
+      cy.log(element.text())
+    });
+  });
+});
